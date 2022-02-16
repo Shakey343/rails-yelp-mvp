709ebdb0272354652c24fd4f024be3946819f255
Jake Pople <jakepople@hotmail.co.uk>
2022-02-16 16:26:19 +0000

    create post request now fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,10 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_path(@review)
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
